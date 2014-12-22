1b56f7fb4c47ef37033b70862dadf103c44f0f46
z1c0 <wziegler@live.at>
2014-12-22 22:26:11 +0100

    Started with CircularFrame

---
+++ Newport/Controls/CircularFrame.cs
@@ -0,0 +1,34 @@
+﻿using System;
+#if UNIVERSAL
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+#else
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Controls;
+#endif
+
+namespace Newport
+{
+#if UNIVERSAL
+  [ContentProperty(Name = "Child")]
+#else
+  [ContentProperty("Child")]
+#endif
+  public class CircularFrame : TemplatedControl
+  {
+    public CircularFrame()
+    {
+      DefaultStyleKey = typeof(CircularFrame);
+    }
+
+    protected override void OnFromTemplate()
+    {
+      var presenter = VerifyGetTemplateChild<ContentPresenter>("ContentPresenter");
+      presenter.Content = Child;
+    }
+
+    public UIElement Child { get; set; }
+  }
+}
